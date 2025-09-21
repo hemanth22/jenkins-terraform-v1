1b4d7fad06161d6b331997bdcec856449e62e99c
Hemanth B <hemanth22hemu@gmail.com>
2025-09-21 15:49:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,115 +1,43 @@
-# Configure the Microsoft Azure Provider.
-
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.45.0"
-    }
-  }
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-
-    virtual_machine {
-      delete_os_disk_on_deletion            = true
-      graceful_shutdown                     = false
-      skip_shutdown_and_force_delete        = false
-    }
-
-  }
-}
-
-# Create a resource group
-resource "azurerm_resource_group" "main" {
-  name     = "jenkins-resources"
-  location = "westus"
-}
-
-# Create virtual network
-resource "azurerm_virtual_network" "main" {
-  name                = "jenkins-vnet"
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-# Create subnet
-resource "azurerm_subnet" "main" {
-  name                 = "jenkins-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# Create public IP Address
-resource "azurerm_public_ip" "main" {
-  name                = "jenkins-public-ip"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Static"
-}
-
-
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "main" {
-  name                = "jenkins-nsg"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-# Create virtual network interface
-resource "azurerm_network_interface" "main" {
-  name                = "jenkins-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "jenkins-ipconfig"
-    subnet_id                     = azurerm_subnet.main.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.main.id
   }
 }
 
-# Create a Linux virtual machine
-
-resource "azurerm_virtual_machine" "main" {
-  name                  = "jenkins-vm"
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_B2s"
-
-  storage_image_reference {
-    publisher = "resf"
-    offer     = "rockylinux-x86_64"
-    sku       = "9-lvm"
-    version   = "9.6.20250531"
-  }
-
-  storage_os_disk {
-    name              = "jenkins-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B2s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
 
   os_profile {
     computer_name  = "master-jenkins"
@@ -117,30 +45,15 @@ resource "azurerm_virtual_machine" "main" {
     admin_password = "Password1234!"
   }
 
-  os_profile_linux_config {
-    disable_password_authentication = false
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
-}
 
-resource "azurerm_virtual_machine_extension" "main" {
-  name                 = "jenkins-init"
-  virtual_machine_id   = azurerm_virtual_machine.main.id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-
-  settings = <<SETTINGS
-    {
-        "commandToExecute": "echo Jenkins"
-    }
-SETTINGS
-
-
-  tags = {
-    environment = "Production"
+  source_image_reference {
+    publisher = "resf"
+    offer     = "rockylinux-x86_64"
+    sku       = "9-lvm"
+    version   = "9.6.20250531"
   }
-}
-
-output "ip" {
-  value = azurerm_public_ip.main.ip_address
 }
